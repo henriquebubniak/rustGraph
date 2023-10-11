cdfc38e8792bcb501652bd7e2e75349d8de55a55
Henrique Bubniak <hg.bubniak@gmail.com>
2023-10-11 09:36:39 -0300

    buba - separated main from module graph

---
+++ src/graph.rs
@@ -0,0 +1,104 @@
+use std::collections::{HashMap, VecDeque};
+
+type Vertex = usize;
+pub struct Graph {
+    adjacencies: HashMap<Vertex, Vec<Vertex>>,
+}
+
+impl Graph {
+    pub fn new() -> Graph {
+        Graph {
+            adjacencies: HashMap::new(),
+        }
+    }
+    pub fn insert_node(&mut self, vertex: Vertex) {
+        self.adjacencies.insert(vertex, Vec::new());
+    }
+    pub fn insert_edge(&mut self, vertex: Vertex, adj: Vertex) {
+        if !self.adjacencies.contains_key(&vertex) {
+            self.insert_node(vertex);
+        }
+        if !self.adjacencies.contains_key(&adj) {
+            self.insert_node(adj);
+        }
+        match self.adjacencies.get_mut(&vertex) {
+            Some(adj_list) => adj_list.push(adj),
+            None => println!("Node not in Graph"),
+        }
+    }
+}
+
+pub struct DFSResult {
+    pub discovery: HashMap<Vertex, usize>,
+    pub finish: HashMap<Vertex, usize>,
+    pub topological_sort: Vec<Vertex>,
+    pub dag: bool,
+}
+impl DFSResult {
+    fn new() -> DFSResult {
+        DFSResult {
+            discovery: HashMap::new(),
+            finish: HashMap::new(),
+            topological_sort: Vec::new(),
+            dag: true,
+        }
+    }
+}
+
+fn dfs_visit(
+    g: &Graph,
+    src: &Vertex,
+    dfs_result: &mut DFSResult,
+    df_time: &mut usize,
+    ts_time: &mut usize,
+) {
+    dfs_result.discovery.insert(*src, *df_time);
+    for v in g.adjacencies.get(src).unwrap() {
+        if dfs_result.discovery.get(v) == Some(&usize::MAX) {
+            *df_time += 1;
+            dfs_visit(g, v, dfs_result, df_time, ts_time)
+        } else if dfs_result.finish.get(v) == Some(&usize::MAX) {
+            dfs_result.dag = false;
+        }
+    }
+    *df_time += 1;
+    dfs_result.finish.insert(*src, *df_time);
+    dfs_result.topological_sort.insert(*ts_time, *src);
+    *ts_time += 1;
+}
+
+pub fn dfs(g: &Graph) -> DFSResult {
+    let mut df_time = 0;
+    let mut ts_time = 0;
+    let mut dfs_result = DFSResult::new();
+    for (vertex, _) in &g.adjacencies {
+        dfs_result.discovery.insert(*vertex, usize::MAX);
+        dfs_result.finish.insert(*vertex, usize::MAX);
+    }
+    for (vertex, _) in g.adjacencies.iter() {
+        if *dfs_result.discovery.get(vertex).unwrap() == usize::MAX {
+            dfs_visit(g, vertex, &mut dfs_result, &mut df_time, &mut ts_time);
+            df_time += 1;
+        }
+    }
+    dfs_result
+}
+
+pub fn bfs(g : &Graph, src : usize) -> Vec<usize> {
+    let mut d = Vec::new();
+    d.resize(g.adjacencies.len(), usize::MAX);
+    d[src] = 0;
+    let mut q = VecDeque::new();
+    q.push_back(src);
+    while !q.is_empty() {
+        let u = q.pop_front().expect("queue empty");
+        let adj_u = g.adjacencies.get(&u).expect("u not in graph");
+        for v in adj_u {
+            if d[*v] == usize::MAX {
+                d[*v] = d[u] + 1;
+                q.push_back(*v)
+            }
+        }
+    }
+    d
+}
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,9 @@
-use std::collections::HashMap;
-
-type Vertex = usize;
-struct Graph {
-    adjacencies: HashMap<Vertex, Vec<Vertex>>,
-}
-
-impl Graph {
-    fn new() -> Graph {
-        Graph {
-            adjacencies: HashMap::new(),
-        }
-    }
-    fn insert_node(&mut self, vertex: Vertex) {
-        self.adjacencies.insert(vertex, Vec::new());
-    }
-    fn insert_edge(&mut self, vertex: Vertex, adj: Vertex) {
-        if !self.adjacencies.contains_key(&vertex) {
-            self.insert_node(vertex);
-        }
-        if !self.adjacencies.contains_key(&adj) {
-            self.insert_node(adj);
-        }
-        match self.adjacencies.get_mut(&vertex) {
-            Some(adj_list) => adj_list.push(adj),
-            None => println!("Node not in Graph"),
-        }
-    }
-}
-
-struct DFSResult {
-    discovery: HashMap<Vertex, usize>,
-    finish: HashMap<Vertex, usize>,
-    topological_sort: HashMap<Vertex, usize>,
-    dag: bool,
-}
-impl DFSResult {
-    fn new() -> DFSResult {
-        DFSResult {
-            discovery: HashMap::new(),
-            finish: HashMap::new(),
-            topological_sort: HashMap::new(),
-            dag: true,
-        }
-    }
-}
-
-fn dfs_visit(
-    g: &Graph,
-    src: &Vertex,
-    dfs_result: &mut DFSResult,
-    df_time: &mut usize,
-    ts_time: &mut usize,
-) {
-    dfs_result.discovery.insert(*src, *df_time);
-    for v in g.adjacencies.get(src).unwrap() {
-        if dfs_result.discovery.get(v) == Some(&usize::MAX) {
-            *df_time += 1;
-            dfs_visit(g, v, dfs_result, df_time, ts_time)
-        } else if dfs_result.finish.get(v) == Some(&usize::MAX) {
-            dfs_result.dag = false;
-        }
-    }
-    *df_time += 1;
-    dfs_result.finish.insert(*src, *df_time);
-    dfs_result.topological_sort.insert(*src, *ts_time);
-    *ts_time += 1;
-}
-
-fn dfs(g: &Graph) -> DFSResult {
-    let mut df_time = 0;
-    let mut ts_time = 0;
-    let mut dfs_result = DFSResult::new();
-    for (vertex, _) in &g.adjacencies {
-        dfs_result.discovery.insert(*vertex, usize::MAX);
-        dfs_result.finish.insert(*vertex, usize::MAX);
-    }
-    for (vertex, _) in &g.adjacencies {
-        if *dfs_result.discovery.get(vertex).unwrap() == usize::MAX {
-            dfs_visit(g, vertex, &mut dfs_result, &mut df_time, &mut ts_time);
-            df_time += 1;
-        }
-    }
-    dfs_result
-}
-
+mod graph;
+use crate::graph::{Graph, dfs};
 fn main() {
     let mut graph = Graph::new();
     graph.insert_edge(0, 1);
+    graph.insert_edge(0, 6);
     graph.insert_edge(1, 2);
     graph.insert_edge(1, 3);
     graph.insert_edge(4, 2);
@@ -100,10 +17,9 @@ fn main() {
         println!("Vertex {} was finished in {}\n", vertex, finish_time);
     }
     println!("Graph is{} a dag\n", if result.dag { "" } else { " not" });
-    for (vertex, top_sort) in result.topological_sort {
-        println!(
-            "Vertex {} position in topological sort is {}\n",
-            vertex, top_sort
-        );
+    println!("Topological sort:\n");
+    for vertex in result.topological_sort {
+        print!("{} ", vertex);
     }
+    print!("\n");
 }
