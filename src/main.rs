146a2804a5bf0068deade7d7d2c04e70f396307a
Henrique Bubniak <hg.bubniak@gmail.com>
2023-10-23 15:02:49 -0300

    Added README.md

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod graph;
 use crate::graph::read_from_file;
 fn main() -> std::io::Result<()> {
-    let mut graph = read_from_file("graph.yaml");
+    let mut graph = read_from_file("dependencies.yaml");
     println!("{:?}", graph);
     graph.insert_edge(6, 7);
     println!("{:?}", graph);
-    graph.save_to_file("graph.yaml");
+    graph.save_to_file("dependencies.yaml");
     Ok(())
 }
