a69408bfc8c07f13139d2d58f6bec388428a9f13
Henrique Bubniak <henriquebubniak@MacBook-Air-de-Henrique.local>
2023-11-01 12:20:36 -0300

    Completed validate function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+use core::fmt;
 use std::collections::HashMap;
+use std::error::Error;
 use std::fs::File;
-use std::io::*;
+use std::io::{Read, Write};
 
 use serde_derive::{Deserialize, Serialize};
 
@@ -9,6 +11,23 @@ pub enum State {
     Visited,
     Finished,
 }
+
+#[derive(Debug)]
+pub enum GraphError {
+    CyclicGraph,
+    InvalidDependency,
+}
+
+impl fmt::Display for GraphError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            GraphError::CyclicGraph => write!(f, "In the remake file there are two targets dependent on one another"),
+            GraphError::InvalidDependency => write!(f, "In the remake file there is a target dependent on a non-existent target"),
+        }
+    }
+}
+
+impl Error for GraphError {}
 
 #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Hash)]
 pub struct Component {
@@ -33,40 +52,75 @@ impl Graph {
             make: HashMap::new(),
         }
     }
-    pub fn read_from_file(&mut self, path: &str) -> Option<Graph> {
-        let mut file = File::open(path).expect("Failed to open file");
+
+    pub fn read_from_file(&mut self, path: &str) -> Result<Graph, Box<dyn Error>> {
+        let mut file = File::open(path)?;
         let mut contents = String::new();
-        file.read_to_string(&mut contents)
-            .expect("Failed to read file contents");
-        let g: Graph = serde_yaml::from_str(&contents).expect("Failed to deserialize graph");
+        file.read_to_string(&mut contents)?;
+        let g: Graph = serde_yaml::from_str(&contents)?;
         match g.validate() {
-            None => Some(g),
-            Some(error) => panic!("{}", error),
+            Some(error) => Err(Box::new(error)),
+            None => Ok(g),
         }
     }
+
     pub fn save_to_file(&self, path: &str) {
         let mut file = File::create(path).expect("Failed to create file");
         let s = serde_yaml::to_string(&self).expect("Failed to serialize graph");
         file.write_all(s.as_bytes()).expect("Failed to write file");
     }
-    pub fn insert_edge(&mut self, target: &str, run: &str, dependencies: Vec<&str>) {
+
+    pub fn insert_edge(
+        &mut self,
+        target: &str,
+        run: &str,
+        dependencies: Vec<&str>,
+    ) -> Option<GraphError> {
         let info = Component::new(run, dependencies);
         self.make.insert(target.to_string(), info);
-        if let Some(error) = self.validate() {
-            panic!("{}", error);
-        }
+        self.validate()
     }
-    fn validate(&self) -> Option<&str> {
-        for (target, adj) in &self.make {
-            for dep in &adj.dependencies {
-                if let None = self.make.get(dep) {
-                    return Some("Dependecy doesn't exist");
-                }
-                if target == dep {
-                    return Some("Target can't depend on itself");
+
+    fn validate(&self) -> Option<GraphError> {
+        let mut st = HashMap::new();
+        for (_, component) in &self.make {
+            for dep in &component.dependencies {
+                if self.make.get(dep) == None {
+                    return Some(GraphError::InvalidDependency);
                 }
             }
+            st.insert(component, State::Unexplored);
+        }
+        let mut cyclic = false;
+        for (_, component) in &self.make {
+            match *st.get(component).expect("Expected state in all components") {
+                State::Unexplored => self.validate_visit(component, &mut st, &mut cyclic),
+                State::Visited => {
+                    panic!("Validate visit is not expected to leave visited but unfinished graphs")
+                }
+                State::Finished => (),
+            };
+        }
+        if cyclic == true {
+            return Some(GraphError::CyclicGraph);
         }
         None
+    }
+    fn validate_visit<'a>(
+        &'a self,
+        component: &'a Component,
+        st: &mut HashMap<&'a Component, State>,
+        cyclic: &mut bool,
+    ) {
+        st.insert(component, State::Visited);
+        for dep in &component.dependencies {
+            let cdep = self.make.get(dep).expect("Validation error");
+            match *st.get(cdep).expect("No state") {
+                State::Visited => *cyclic = true,
+                State::Unexplored => self.validate_visit(cdep, st, cyclic),
+                State::Finished => (),
+            };
+        }
+        st.insert(component, State::Finished);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
-use grph::{Graph, Component, State};
-use std::{env, collections::HashMap};
-use execute::{Execute, shell};
+use execute::{shell, Execute};
+use grph::{Component, Graph};
 use std::process::Stdio;
+use std::env;
 
-fn make<'a>(g: &'a Graph, c: &'a Component, st: &mut HashMap<&'a Component, State>) {
-    st.insert(c, State::Visited);
+fn make<'a>(g: &'a Graph, c: &'a Component) {
     for dep in &c.dependencies {
-        let cdep = g.make.get(dep).expect("Validation error");
-        match *st.get(cdep).expect("No state") {
-            State::Visited => panic!("Cant have cycle in dependencies"),
-            State::Unexplored => make(g, cdep, st),
-            State::Finished => ()
-        }
+        let cdep = g.make.get(dep).expect("All dependencies are expected to exist");
+        make(g, cdep);
     }
-    st.insert(c, State::Finished);
     println!("{}", c.run);
     let mut command = shell(&c.run[..]);
     command.stdout(Stdio::piped());
@@ -25,17 +19,22 @@ fn main() {
     match target.len() {
         1 => {
             let target = &target[0];
-            println!("{:?}", target);
-            let g = Graph::new()
-                .read_from_file("deps.yaml")
-                .expect("A target depends on non existing target");
-            let c = g.make.get(target).expect("Target non existent");
-            let mut state_tracker = HashMap::new();
-            for (_, c) in &g.make {
-                state_tracker.insert(c, State::Unexplored);
-            }
-            make(&g, c, &mut state_tracker);
+            let g = match Graph::new().read_from_file("deps.yaml") {
+                Ok(g) => g,
+                Err(e) => {
+                    println!("{}", e);
+                    return;
+                }
+            };
+            let c = match g.make.get(target) {
+                None => {
+                    println!("Non-existing target");
+                    return;
+                }
+                Some(c) => c
+            };
+            make(&g, c);
         }
-        _ => ()
+        _ => (),
     }
 }
